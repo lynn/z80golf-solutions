5255e40d1c59fa50ea1c97b08aa4c81f63a94bf7
Lynn <lynn@foldr.moe>
2018-05-28 17:51:41 +0200

    Update Implement+Pluso.asm

--- Implement+Pluso.asm
+++ Implement+Pluso.asm
@@ -1,18 +1,19 @@
-R:push de
-  call $8003
-  jr nc, S
+R:push de     ; Push $0000
+  call $8003  ; Call getchar()
+  jr nc, S    ; Halt on EOF
   halt
 S:cp 'o'
-  jr c, R
-  jr z, T
-  inc b
-  jr R
-T:ld a, b
-  cp 26
-  jr c, U
+  jr c, R   ; Ignore below 'o'
+  jr z, O   ; Jump to output code if == 'o'
+  inc b     ; Otherwise ('p'), increment the Pluso accumulator
+  jr R      ; and start from top.
+O:ld a, b   ; Load Pluso accumulator
+  cp 26     ; If it's >= 26, wrap to [-1, 25] range
+  jr c, T
   sub 27
   ld b, a
-U:cp -1
-  jr nz, V
-  add 1-'A'+' '
-V:add 'A'
+T:cp -1     ; Map -1 to space, others to 'A'..'Z'
+  jr nz, U
+  add 1+' '-'A'
+U:add 'A'
+            ; Fall through to $8000 = putchar() then return to pushed $0000.
